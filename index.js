a5df4fe1d5ff7a19177cb2113b76432bc3f3ce59
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-03-28 12:36:28 -0300

    Add logging function

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
 (async function () {
+  const LOG_DEBUG = true;
+
+  /**
+   * Creates a console logging function with log level highlighting.
+   * 
+   * @param {"log" | "error" | "warn" | "info" | "debug"} logLevel 
+   *  Log levels available on the `console` object.
+   * @param {boolean} enabled 
+   *  Turn console output on or off. Best used with environment variables.
+   * @returns {(...args: any[]) => void}
+   * @author Benjamin P.C. Hovinga
+   * @license MIT
+   */
+  function createLogger(logLevel = "log", enabled = true) {
+      // Define console colors for each log level
+      const colors = {
+          "log": "\x1b[30m", // BLACK
+          "error": "\x1b[31m", // RED
+          "warn": "\x1b[33m", // YELLOW
+          "info": "\x1b[36m", // CYAN
+          "debug": "\x1b[35m", // MAGENTA
+      };
+      const reset = "\x1b[0m";  // RESET COLOR
+
+      // Validate logLevel
+      if (!colors.hasOwnProperty(logLevel))
+          throw TypeError(`${logLevel} is not a valid logLevel.`);
+
+      // Build prefix string
+      const prefix = `${colors[logLevel]}[${logLevel.toUpperCase()}]${reset}`;
+
+      // Return logger function
+      return function (...args) {
+          if (enabled) console[logLevel](prefix, ...args);
+      }
+  }
+
+  // Create console loggers
+  const debug = createLogger("debug", LOG_DEBUG);
+  const info = createLogger("info");
+  const error = createLogger("error");
+
+
   class Level {
     /** All possible game levels */
     static LEVELS = Object.freeze([
@@ -22,9 +65,11 @@
       this.selectorElement = document.getElementById('gameLevelSelect');
       if (!this.selectorElement || !(this.selectorElement instanceof HTMLSelectElement))
         throw new Error("Missing HTML select element 'gameLevelSelect'");
+      debug("Level selector element:", this.selectorElement);
 
       // Make sure the level selectors value is in sync with the current level
       this.currentLevel = this.selectorElement.value;
+      debug("Initial level:", this.currentLevel);
 
       // Add onChange event listener to the level selector
       this.selectorElement.addEventListener('change', (e) => {this.onChange(e);});
