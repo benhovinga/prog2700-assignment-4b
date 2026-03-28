dc18bdc7a530cc10adce6f804fa1b1ee751e9da3
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-03-28 12:34:45 -0300

    Refactor level and add comments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-(function () {
-  class LevelController {
-    allLevels = Object.freeze([
+(async function () {
+  class Level {
+    /** All possible game levels */
+    static LEVELS = Object.freeze([
       '6x6',
       '8x8',
       '10x10',
@@ -9,34 +10,64 @@
       'sample',
       'random',
     ]);
+
+    /** @type {HTMLSelectElement} */
+    selectorElement;
+
     /** @type {String} */
-    #currentLevel = this.allLevels[0];  // Default to first level
+    #currentLevel = Level.LEVELS[0];  // Default to first level
 
     constructor() {
+      // Get and validate the level selector element
       this.selectorElement = document.getElementById('gameLevelSelect');
-      if (!this.selectorElement)
-        throw new Error("Missing HTML element 'gameLevelSelect'");
+      if (!this.selectorElement || !(this.selectorElement instanceof HTMLSelectElement))
+        throw new Error("Missing HTML select element 'gameLevelSelect'");
 
-      this.setCurrentLevel(this.selectorElement.value);
-      this.selectorElement.addEventListener('change', this.onLevelChange);
+      // Make sure the level selectors value is in sync with the current level
+      this.currentLevel = this.selectorElement.value;
+
+      // Add onChange event listener to the level selector
+      this.selectorElement.addEventListener('change', (e) => {this.onChange(e);});
     }
 
-    getCurrentLevel() {
+    /**
+     * Validates the level is in the levels list.
+     * @param {string} level 
+     */
+    static isLevelValid(level) {
+      return typeof level === 'string' && Level.LEVELS.includes(level);
+    }
+
+    /** Get the current level. */
+    get currentLevel() {
       return this.#currentLevel;
     }
 
-    /** @param {String} value  */
-    setCurrentLevel(value) {
-      if (!value || typeof value !== 'string' || !this.allLevels.includes(value))
-        throw new Error(`Invalid level value "${value}"`);
+    /** 
+     * Set the current level to a new level.
+     * 
+     * @param {String} value
+     */
+     set currentLevel(value) {
+      if (!Level.isLevelValid(value))
+        throw new Error(`Invalid level "${value}"`);
       this.#currentLevel = value;
     }
 
-    onLevelChange(event) {
-      this.setCurrentLevel(event.target.value);
+    /** 
+     * Change event listener for the level selector.
+     * @param {Event} event
+     */
+    onChange(event) {
+      if(!event.target || !(event.target instanceof HTMLSelectElement))
+        throw new Error("Event target must be instance of HTMLSelectElement");
+      // TODO: Load the puzzle from the API
+
+      // TODO: Redraw the game board
+      // Update the current level
+      this.currentLevel = event.target.value;
+      info("New level selected:", this.currentLevel);
     }
   }
-
-  const Level = new LevelController();
-
+  const gameLevel = new Level();
 })();
